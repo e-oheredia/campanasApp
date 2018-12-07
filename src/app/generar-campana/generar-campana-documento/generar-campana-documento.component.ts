13ba3a97e90788146b81b192153f6b5f6e66e834
cbaltazar7 <cbaltazar@exact.com.pe>
2018-12-06 20:04:41 -0500

    avance-costo-campana

--- src/app/generar-campana/generar-campana-documento/generar-campana-documento.component.ts
+++ src/app/generar-campana/generar-campana-documento/generar-campana-documento.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Plazo } from 'src/app/model/plazo.model';
 import { PlazoService } from 'src/app/services/plazo.service';
 import { TipoDocumento } from 'src/app/model/tipodocumento.model';
-import { TipoDocumentoService} from 'src/app/services/tipodocumento.service';
+import { TipoDocumentoService } from 'src/app/services/tipodocumento.service';
 import { TipoDestino } from 'src/app/model/tipodestino.model';
 import { TipoDestinoService } from 'src/app/services/tipodestino.service';
 import { PaqueteHabilitado } from 'src/app/model/paquetehabilitado.model';
@@ -11,6 +11,8 @@ import { TipoAgrupado } from 'src/app/model/tipoagrupado.model';
 import { TipoAgrupadoService } from 'src/app/services/tipoagrupado.service';
 import { AccionRestosProveedor } from 'src/app/model/accionrestosproveedor.model';
 import { AccionRestosProveedorService } from 'src/app/services/accionrestosproveedor.service';
+import { CentroCostos } from 'src/app/model/centrocostos.model';
+import { GrupoCentroCostos } from 'src/app/model/grupocentrocostos.model';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
@@ -22,78 +24,95 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class GenerarCampanaDocumentoComponent implements OnInit {
 
-  constructor(private plazoService:PlazoService,
-              private tipoDocumentoService:TipoDocumentoService,
-              private tipoDestinoService:TipoDestinoService,
-              private paqueteHabilitadoService:PaqueteHabilitadoService,
-              private tipoAgrupadoService:TipoAgrupadoService,
-              private accionRestosProveedorService:AccionRestosProveedorService) { }
+  constructor(private plazoService: PlazoService,
+    private tipoDocumentoService: TipoDocumentoService,
+    private tipoDestinoService: TipoDestinoService,
+    private paqueteHabilitadoService: PaqueteHabilitadoService,
+    private tipoAgrupadoService: TipoAgrupadoService,
+    private accionRestosProveedorService: AccionRestosProveedorService) { }
 
 
 
   campanaForm: FormGroup;
-              
-  plazos:Plazo[]=[];
-  tiposDocumento:TipoDocumento[]=[];
-  tiposDestino:TipoDestino[]=[];
-  paqueteHabilitados:PaqueteHabilitado[]=[];
-  tiposAgrupado:TipoAgrupado[]=[];
-  accionesRestosProveedor:AccionRestosProveedor[]=[];
 
+  plazos: Plazo[] = [];
+  tiposDocumento: TipoDocumento[] = [];
+  tiposDestino: TipoDestino[] = [];
+  paqueteHabilitados: PaqueteHabilitado[] = [];
+  tiposAgrupado: TipoAgrupado[] = [];
+  accionesRestosProveedor: AccionRestosProveedor[] = [];
+  
+  centroCostosList : CentroCostos[] = [];
+    
+  grupoCentroCostos : GrupoCentroCostos;
+
+  //: number = 1; //1 BCP - 2 OTROS
 
   ngOnInit() {
     this.cargarVista();
     this.campanaForm = new FormGroup({
-      'nombreCampana' : new FormControl("", Validators.required)
+      'nombreCampana': new FormControl("", Validators.required),
+      'costoCampana': new FormControl(),
+      'cuentaContable' : new FormControl(),
+      'centroCostos' : new FormControl(),
+      'ordenEstadistica' : new FormControl(),
+      'grupoArticulo' : new FormControl(),
+      'porcentajePago' : new FormControl()
     })
   }
 
-  cargarVista(){
+  cargarVista() {
     this.listarTiposDocumento();
     this.listarPlazos();
     this.listarTiposDestino();
     this.listarHabilitados();
     this.listarTiposAgrupado();
     this.listarAccionRestosProveedor();
+    this.grupoCentroCostos = new GrupoCentroCostos(this.centroCostosList);
   }
 
 
-  listarTiposDocumento(){
+  listarTiposDocumento() {
     this.tipoDocumentoService.listarAll().subscribe(
-      tiposdocumento => {this.tiposDocumento = tiposdocumento}
+      tiposdocumento => { this.tiposDocumento = tiposdocumento }
     )
   }
 
-  listarPlazos(){
+  listarPlazos() {
     this.plazoService.listarAll().subscribe(
-      plazos => {this.plazos = plazos}
+      plazos => { this.plazos = plazos }
     )
   }
 
-  listarTiposDestino(){
+  listarTiposDestino() {
     this.tipoDestinoService.listarAll().subscribe(
-      tiposdestino => { this.tiposDestino = tiposdestino}
+      tiposdestino => { this.tiposDestino = tiposdestino }
     )
   }
 
-  listarHabilitados(){
+  listarHabilitados() {
     this.paqueteHabilitadoService.listarAll().subscribe(
-      paquetehabilitado => { this.paqueteHabilitados = paquetehabilitado}
+      paquetehabilitado => { this.paqueteHabilitados = paquetehabilitado }
     )
   }
-  
-  listarTiposAgrupado(){
+
+  listarTiposAgrupado() {
     this.tipoAgrupadoService.listarAll().subscribe(
-      tiposagrupado => { this.tiposAgrupado = tiposagrupado}
+      tiposagrupado => { this.tiposAgrupado = tiposagrupado }
     )
   }
 
-  listarAccionRestosProveedor(){
+  listarAccionRestosProveedor() {
     this.accionRestosProveedorService.listarAll().subscribe(
-      accionesrestoproveedor => { this.accionesRestosProveedor = accionesrestoproveedor}
+      accionesrestoproveedor => { this.accionesRestosProveedor = accionesrestoproveedor }
     )
   }
 
+  agregarCentroCostoItem() {
 
+    let cc = new CentroCostos(null,this.campanaForm.get("cuentaContable").value,this.campanaForm.get("centroCostos").value,this.campanaForm.get("ordenEstadistica").value,this.campanaForm.get("grupoArticulo").value,this.campanaForm.get("porcentajePago").value);
+    this.grupoCentroCostos.centroscostos.push(cc);
+    
+  }
 
 }
